0070f208c7e76d60a41635380899d6e26e59e615
ryu20010202 <apo200187@gmail.com>
2020-11-26 14:36:18 +0900

    共有header

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -2,8 +2,21 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/reset.css') }}">
     <title>codewiki</title>
 </head>
+<header>
+  <div class="top">
+    <div class="title-top">
+      <h1>Code Wiki</h1>
+    </div>
+    <ul class="ul-top">
+      <li class="li-top">ログイン</li>
+      <li class="li-top">新規登録</li>
+    </ul>
+  </div>
+</header>
 <body>
 
     @yield('content')
